f29d0dc9956008ed2a4ea652a2e5b3a00ebca2a5
Julien <caron-julien@live.fr>
2018-10-21 01:10:27 +0200

    Remove Keybord file, add event TouchClicked

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,15 +1,57 @@
 import React, {Component} from 'react';
 import Result from './Result';
-import Keyboard from './Keyboard';
 import './Calculator.css';
+import Touch from "./Touch";
+
+const TouchList = [
+    {text: 'AC', style: ''},
+    {text: '+/-', style: ''},
+    {text: '%', style: ''},
+    {text: '/', style: 'Touch--orange'},
+    {text: '7', style: ''},
+    {text: '8', style: ''},
+    {text: '9', style: ''},
+    {text: 'X', style: 'Touch--orange'},
+    {text: '4', style: ''},
+    {text: '5', style: ''},
+    {text: '6', style: ''},
+    {text: '-', style: 'Touch--orange'},
+    {text: '1', style: ''},
+    {text: '2', style: ''},
+    {text: '3', style: ''},
+    {text: '+', style: 'Touch--orange'},
+    {text: '0', style: 'Touch--0'},
+    {text: ',', style: ''},
+    {text: '=', style: 'Touch--orange'},
+];
 
 class Calculator extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {result: 0};
+    }
+
+    touchClicked(text) {
+        console.log(text,'clicked')
+    }
+
     render() {
         return (
             <div className="Calculator">
                 <div className="container">
-                    <Result result={0}/>
-                    <Keyboard/>
+                    <Result result={this.state.result}/>
+
+                    <div className="grid">
+                        {TouchList.map(({text, style}, index) => (
+                            <Touch
+                                text={text}
+                                style={style}
+                                key={index}
+                                touchClick={this.touchClicked.bind(this)}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         );
--- src/Touch.js
+++ src/Touch.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Touch = ({text, style}) => <div className={'Touch ' + style}>{text}</div>;
+const Touch = ({text, style, touchClick}) => (
+    <div className={'Touch ' + style} onClick={() => touchClick(text)}>{text}</div>
+);
 
 Touch.propTypes = {
     text: PropTypes.string,
-    style: PropTypes.string
+    style: PropTypes.string,
+    touchClick: PropTypes.func
 };
 
 Touch.defaultProps = {text: '', style: ''};
