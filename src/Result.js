72cefcc3673e54a0e66c0c7f7b361100dd9f52ed
Julien <caron-julien@live.fr>
2019-04-22 14:01:47 +0200

    Updates

--- src/Result.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-const Result = ({result}) => <div className="Result">{result}</div>;
-
-Result.propTypes = {result: PropTypes.string.isRequired};
-Result.propDefault = {result: '0'};
-
-export default Result;
